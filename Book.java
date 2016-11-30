ede921c532955d1bf567c3d57b6c40102867cff0
sabrina-wen <sabrinawn253@gmail.com>
2016-11-30 03:21:56 -0500

    v2

--- Book.java
+++ Book.java
@@ -1,12 +1,10 @@
 public class Book {
-    String author;
-    String title;
-    int ISBN;
+    private String author;
+    private String title;
+    private int ISBN;
 
     public Book() {
-	author;
-	title;
-	ISBN;
+	// null constructor
     }
     
     public Book (String author, String title, int ISBN) {
