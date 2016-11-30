33049a9a960064f1bdaae9163acfe63c8738e6fd
sabrina-wen <sabrinawn253@gmail.com>
2016-11-30 03:11:09 -0500

    v1

---
+++ Book.java
@@ -0,0 +1,17 @@
+public class Book {
+    String author;
+    String title;
+    int ISBN;
+
+    public Book() {
+	author;
+	title;
+	ISBN;
+    }
+    
+    public Book (String author, String title, int ISBN) {
+	this.author = author;
+	this.title = title;
+	this.ISBN = ISBN;
+    }
+}
