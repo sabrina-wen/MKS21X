0e8a060cfbba8789536728cb0532af386b0a4b36
sabrina-wen <sabrinawn253@gmail.com>
2016-11-30 05:35:30 -0500

    v3 i think?

---
+++ 01Library/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private String author;
+    private String title;
+    private int ISBN;
+
+    public Book() {
+	// empty constructor woo
+    }
+    
+    public Book (String author, String title, int ISBN) {
+	this.author = author;
+	this.title = title;
+	this.ISBN = ISBN;
+    }
+}
