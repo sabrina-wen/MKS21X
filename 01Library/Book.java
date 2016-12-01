3b28c73c2218029f48fe37691f2459cb0bfee069
sabrina-wen <sabrinawn253@gmail.com>
2016-12-01 01:36:18 -0500

    fixed up weird code

--- 01Library/Book.java
+++ 01Library/Book.java
@@ -1,34 +1,20 @@
 public class Book {
     private String author;
     private String title;
-<<<<<<< HEAD
-    private int ISBN;
+    private String ISBN;
 
     public Book() {
 	// null constructor
     }
     
-    public Book (String author, String title, int ISBN) {
-=======
-    private String ISBN;
-
-    public Book() {
-	// empty constructor woo
-    }
-    
     public Book (String author, String title, String ISBN) {
->>>>>>> c64d8cc9c84b83bcbbd8f316f9cacec382a84712
 	this.author = author;
 	this.title = title;
 	this.ISBN = ISBN;
     }
 
     public String getAuthor() {
-<<<<<<< HEAD
 	return author;
-=======
-        return author;
->>>>>>> c64d8cc9c84b83bcbbd8f316f9cacec382a84712
     }
 
     public void setAuthor(String newAuthor) {
@@ -39,19 +25,15 @@ public class Book {
 	return title;
     }
 
-<<<<<<< HEAD
     public void setTitle(String newTitle) {
 	title = newTitle;
     }
 
-    public int getISBN() {
+    public String getISBN() {
 	return ISBN;
     }
 
     public void setISBN (int newISBN) {
 	ISBN = newISBN;
     }
-=======
-
->>>>>>> c64d8cc9c84b83bcbbd8f316f9cacec382a84712
 }
