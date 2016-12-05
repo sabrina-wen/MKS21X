e0253041b9d23abfc6022e7e0e33449218bd6cd6
sabrina-wen <sabrinawn253@gmail.com>
2016-12-04 22:21:08 -0500

    added hasNext(), next() and remove(). error in throwing nosuchelementexcpetion

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -1,23 +1,33 @@
 import java.util.Iterator;
 
 public class SuperArrayIterator implements Iterable<String> {
-    private String[][] data;
+    private SuperArray ary;
     private int pos;
     
     // superarrayiterator needs to know which superarray is it looking at,
     // need refence to superarray -->
     // store in instance variable, need to initialize variable
 
-    SuperArray data = new SuperArray();
-
-    public SuperArrayIterator(String[] data) {
-	this.data = data;
+    public SuperArrayIterator(SuperArray data) {
+	ary = data;
 	pos = 0;
     }
 
     public boolean hasNext() {
-	return pos < data.length() - 1;
+	return pos < ary.size() - 1;
     }
 
-    
+    public String next() {
+	if ( hasNext() ) {
+	    pos++;
+	    return ary.get(pos + 1);
+	}
+	else {
+	    throw new NoSuchElementException();
+	}
+    }
+
+    public void remove() {
+	throw new UnsupportedOperationException();
+    }
 }
