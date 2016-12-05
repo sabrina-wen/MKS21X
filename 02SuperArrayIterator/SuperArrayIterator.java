f0cfde557a9468f07e4cb88ce29e9219b4065678
sabrina-wen <sabrinawn253@gmail.com>
2016-12-04 22:08:24 -0500

    added pos variable and String array, edited constructor, added hasNext()

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -1,13 +1,23 @@
 import java.util.Iterator;
 
 public class SuperArrayIterator implements Iterable<String> {
+    private String[][] data;
+    private int pos;
+    
     // superarrayiterator needs to know which superarray is it looking at,
     // need refence to superarray -->
     // store in instance variable, need to initialize variable
 
     SuperArray data = new SuperArray();
 
-    public SuperArrayIterator() {
+    public SuperArrayIterator(String[] data) {
 	this.data = data;
+	pos = 0;
     }
+
+    public boolean hasNext() {
+	return pos < data.length() - 1;
+    }
+
+    
 }
