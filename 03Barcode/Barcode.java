000aafc23c3f8a0778e0c3d49f21bd1ca89716d6
sabrina-wen <sabrinawn253@gmail.com>
2016-12-07 22:30:32 -0500

    constructor works

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,4 +1,5 @@
-public class Barcode implements Comparable<Barcode>{
+public class Barcode {
+//implements Comparable<Barcode> {
 // instance variables
    private String _zip;
    private int _checkDigit;
@@ -11,27 +12,39 @@ public class Barcode implements Comparable<Barcode>{
   public Barcode(String zip) {
       _zip = zip;
       _checkDigit = 0;
-      if (zip.length() != 5) {
-	  throw new 
+      if (_zip.length() != 5) {
+	  throw new IllegalArgumentException();
+      }
+      for (int i = 0; i < _zip.length(); i++) {
+	  if ( (_zip.charAt(i) < '0') || (_zip.charAt(i) > '9') ) {
+	      throw new IllegalArgumentException();
+	  }
+      }
   }
 
 // postcondition: Creates a copy of a bar code.
-  public Barcode clone() {
-  }
+    //public Barcode clone() {
+    //}
 
 
 // postcondition: computes and returns the check sum for _zip
-  private int checkSum() {
-  }
+    // private int checkSum() {
+    // }
 
 //postcondition: format zip + check digit + Barcode 
 //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
-  public String toString() {
-  }
+    // public String toString() {
+    // }
 
 
 // postcondition: compares the zip + checkdigit, in numerical order. 
-  public int compareTo(Barcode other) {
-  }
-    
+    // public int compareTo(Barcode other) {
+    //}
+
+    public static void main (String[] args) {
+	Barcode uno = new Barcode("18345");
+	System.out.println(uno);
+	// Barcode dos = new Barcode("1&204"); should be an  error
+	// Barcode tres = new Barcode("hey!!!!!"); should be an error
+    }
 }
