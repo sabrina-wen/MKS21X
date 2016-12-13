0ea05005a75bcaf8bde721b301c767131216dfd0
sabrina-wen <sabrinawn253@gmail.com>
2016-12-13 05:34:02 -0500

    initial file w/ name method done

---
+++ 04Sorts/Sorts.java
@@ -0,0 +1,15 @@
+public class Sorts{
+  /**EDIT THIS METHOD TO MATCH YOUR NAME 
+  */
+  public static String name() {
+    return "09.Wen.Sabrina"; 
+  }
+
+  /**Selection sort of an int array. 
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data the elements to be sorted.
+  */
+  public static void selectionSort(int[] data) { 
+  }
+
+}
